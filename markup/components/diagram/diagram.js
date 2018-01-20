16abf6688a987d903474b0e62a644fe145cfa493
Viktor Voronin <mobimrzg@gmail.com>
2018-01-20 14:25:03 +0500

    before modals start

--- markup/components/diagram/diagram.js
+++ markup/components/diagram/diagram.js
@@ -6,22 +6,33 @@
 //         }
 //     };
 // }
-
 const diagram = document.querySelector('.diagram');
-const floorsBlock = document.querySelector('.floors');
-const floorsBlockHeight = parseInt(
-    window.getComputedStyle(floorsBlock).height,
-    10
-);
-const hoursBarStyle = document.createElement('style');
-hoursBarStyle.innerHTML = `.hours__bar {height: ${floorsBlockHeight}px;} .diagram__curr-time-label::after { height: ${floorsBlockHeight +
-    8}px;}`;
-document.body.appendChild(hoursBarStyle);
-
+const setVerticalBarsHeigth = () => {
+    const floorsBlock = document.querySelector('.floors');
+    const floorsBlockHeight = parseInt(
+        window.getComputedStyle(floorsBlock).height,
+        10
+    );
+    const currHourLabelVerticalOffset =
+        document.body.offsetWidth < 960 ? 8 : 16;
+    const hoursBarStyle = document.createElement('style');
+    hoursBarStyle.innerHTML = `.hours__bar {height: ${floorsBlockHeight}px;} .diagram__curr-time-label::after { height: ${floorsBlockHeight +
+        currHourLabelVerticalOffset}px;}`;
+    document.body.appendChild(hoursBarStyle);
+};
 const fixFloorLabels = () => {
+    if (document.body.offsetWidth >= 960) {
+        return;
+    }
     const floorLabels = document.querySelectorAll('.floor__label');
     floorLabels.forEach(fl => {
         fl.style.left = `${diagram.scrollLeft + 16}px`;
     });
 };
 diagram.addEventListener('scroll', fixFloorLabels);
+window.addEventListener('resize', setVerticalBarsHeigth);
+document.addEventListener('DOMContentLoaded', setVerticalBarsHeigth);
+
+// fix  vertical bars heights on diagram change
+let observer = new MutationObserver(setVerticalBarsHeigth);
+observer.observe(diagram, { childList: true });
